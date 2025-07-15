7725086ba434a8f6081c19718efdd5eec5a3a139
AbhinandNR <AbhinandNR>
2025-07-15 22:41:50 +0530

    commited

--- src/components/Booknav.jsx
+++ src/components/Booknav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Booknav = () => {
   return (
     <div><nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <div class="container-fluid bg-muted">
     <a class="navbar-brand" href="#">Books</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -25,9 +25,7 @@ const Booknav = () => {
         <li class="nav-item">
           <a class="nav-link" href="#">DeleteBook</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
     </div>
   </div>
